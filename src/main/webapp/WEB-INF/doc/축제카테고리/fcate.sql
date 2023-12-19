772d1bf8e684c1a35bd4be6c43bbf04d295acb20
hyeonsu6 <bhshs0508@naver.com>
2023-12-20 01:34:36 +0900

    카테고리 수정

--- src/main/webapp/WEB-INF/doc/축제카테고리/fcate.sql
+++ src/main/webapp/WEB-INF/doc/축제카테고리/fcate.sql
@@ -49,69 +49,51 @@ COMMIT;
 
 -- READ: LIST
 SELECT * FROM FCATE;
-SELECT FCATEno, name, cnt, rdate, seqno, visible FROM FCATE ORDER BY FCATEno ASC;
-    FCATENO NAME                                  CNT RDATE                    SEQNO V
----------- ------------------------------ ---------- ------------------- ---------- -
-         1 경기도                                  0 2023-09-13 12:18:46          1 N
-         2 강원도                                  0 2023-09-13 12:18:46          1 N
-         3 충청남도                                0 2023-09-13 12:18:46          1 N
+SELECT FCATEno, name, cnt, rdate, seqno, visible FROM FCATE ORDER BY fcateno ASC;
 
 -- READ
-SELECT FCATEno, name, cnt, rdate FROM FCATE WHERE FCATEno=1;
-    FCATENO NAME                                  CNT RDATE              
----------- ------------------------------ ---------- -------------------
-         1 경기도                                  0 2023-09-06 12:09:45
-         
+SELECT fcateno, name, cnt, rdate FROM FCATE WHERE fcateno=1;
+        
 -- UPDATE
 UPDATE FCATE SET name='전라도', cnt=1 WHERE FCATEno=1;
-    FCATENO NAME                                  CNT RDATE              
----------- ------------------------------ ---------- -------------------
-         1 전라도                                  1 2023-09-06 12:09:45
 
 -- DELETE
-DELETE FROM FCATE WHERE FCATEno=7;
-DELETE FROM FCATE WHERE FCATEno >= 10;
+DELETE FROM FCATE WHERE fcateno=7;
+DELETE FROM FCATE WHERE fcateno >= 10;
 
 COMMIT;
 
 -- COUNT
 SELECT COUNT(*) as cnt FROM FCATE;
-       CNT
-----------
-         2
 
 -- 우선 순위 높임, 10 등 -> 1 등
-UPDATE FCATE SET seqno = seqno - 1 WHERE FCATEno=1;
-SELECT FCATEno, name, cnt, rdate, seqno FROM FCATE ORDER BY FCATEno ASC;
+UPDATE FCATE SET seqno = seqno - 1 WHERE fcateno=1;
+SELECT fcateno, name, cnt, rdate, seqno FROM FCATE ORDER BY fcateno ASC;
 
 -- 우선 순위 낮춤, 1 등 -> 10 등
-UPDATE FCATE SET seqno = seqno + 1 WHERE FCATEno=1;
-SELECT FCATEno, name, cnt, rdate, seqno FROM FCATE ORDER BY FCATEno ASC;
+UPDATE FCATE SET seqno = seqno + 1 WHERE fcateno=1;
+SELECT fcateno, name, cnt, rdate, seqno FROM FCATE ORDER BY fcateno ASC;
 
 -- READ: LIST
-SELECT FCATEno, name, cnt, rdate, seqno FROM FCATE ORDER BY seqno ASC;
+SELECT fcateno, name, cnt, rdate, seqno FROM FCATE ORDER BY seqno ASC;
 
 COMMIT;
 
 -- 카테고리 공개 설정
-UPDATE FCATE SET visible='Y' WHERE FCATEno=1;
-SELECT FCATEno, name, cnt, rdate, seqno, visible FROM FCATE ORDER BY FCATEno ASC;
+UPDATE FCATE SET visible='Y' WHERE fcateno=1;
+SELECT fcateno, name, cnt, rdate, seqno, visible FROM FCATE ORDER BY fcateno ASC;
 
 -- 카테고리 비공개 설정
-UPDATE FCATE SET visible='N' WHERE FCATEno=1;
-SELECT FCATEno, name, cnt, rdate, seqno, visible FROM FCATE ORDER BY FCATEno ASC;
+UPDATE FCATE SET visible='N' WHERE fcateno=1;
+SELECT fcateno, name, cnt, rdate, seqno, visible FROM FCATE ORDER BY fcateno ASC;
 
 COMMIT;
 
 -- 비회원/회원 SELECT LIST, id: list_all_y
-SELECT FCATEno, name, cnt, rdate, seqno, visible 
+SELECT fcateno, name, cnt, rdate, seqno, visible 
 FROM FCATE 
 WHERE visible='Y'
 ORDER BY seqno ASC;
-
-    FCATENO NAME                                  CNT RDATE                    SEQNO V
----------- ------------------------------ ---------- ------------------- ---------- -
-         2 강원도                                  0 2023-09-13 12:18:46          1 Y
 
 
          
