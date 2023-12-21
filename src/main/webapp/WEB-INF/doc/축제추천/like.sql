32a8ebac2bcb9ba0e1c67ef55d90e4d30a7ccea0
hyeonsu6 <bhshs0508@naver.com>
2023-12-22 00:32:47 +0900

    likes

--- src/main/webapp/WEB-INF/doc/축제추천/like.sql
+++ src/main/webapp/WEB-INF/doc/축제추천/like.sql
@@ -1,55 +1,47 @@
-DROP TABLE GOOD;   
+DROP TABLE LIKES;   
 
-CREATE TABLE GOOD(
-        goodno                 NUMBER(10)         NOT NULL         PRIMARY KEY,
-        memberno             NUMBER(10)         NULL ,
+CREATE TABLE LIKES(
+        likesno                 NUMBER(10)         NOT NULL         PRIMARY KEY,
         contentsno           NUMBER(10)         NULL ,
-        RDATE                DATE         NOT NULL,
-  FOREIGN KEY (memberno) REFERENCES member (memberno),
-  FOREIGN KEY (contentsno) REFERENCES festival (contentsno)
+        memberno             NUMBER(10)         NULL ,
+        rdate                DATE         NOT NULL,
+  FOREIGN KEY (contentsno) REFERENCES festival (contentsno),
+  FOREIGN KEY (memberno) REFERENCES member (memberno)
 );
 
-COMMENT ON TABLE good is '축제/행사 컨텐츠 추천';
-COMMENT ON COLUMN good.goodno is '좋아요 번호';
-COMMENT ON COLUMN good.memberno is '회원번호';
-COMMENT ON COLUMN good.contentsno is '컨텐츠번호';
-COMMENT ON COLUMN good.RDATE is '추천 날짜';
+COMMENT ON TABLE LIKES is '축제 컨텐츠 추천';
+COMMENT ON COLUMN LIKES.likesno is '좋아요 번호';
+COMMENT ON COLUMN LIKES.contentsno is '회원 번호';
+COMMENT ON COLUMN LIKES.memberno is '축제 컨텐츠 번호';
+COMMENT ON COLUMN LIKES.rdate is '등록일';
 
-DROP SEQUENCE GOOD_SEQ;
+DROP SEQUENCE LIKES_SEQ;
 
-CREATE SEQUENCE GOOD_SEQ
+CREATE SEQUENCE LIKES_SEQ
   START WITH 1              -- 시작 번호
   INCREMENT BY 1          -- 증가값
   MAXVALUE 9999999999 -- 최대값: 9999999 --> NUMBER(7) 대응
   CACHE 2                       -- 2번은 메모리에서만 계산
   NOCYCLE;                     -- 다시 1부터 생성되는 것을 방지
+  
+-- 등록
+INSERT INTO likes(likesno, contentsno, memberno, rdate) 
+VALUES (LIKES_SEQ.nextval, 1, 3, sysdate);
 
---1) 기존에 추천을 했는지 확인하는 쿼리, 1: 좋아요 클릭함, 0: 아직 좋아요 클릭안함.
-SELECT COUNT(*) as cnt
-FROM good
-WHERE memberno = 1 AND contentsno=1;
+-- 조회 : List
+SELECT * FROM likes;
+SELECT likesno, contentsno, memberno, rdate FROM likes ORDER BY likesno ASC;
 
---2) 좋아요 처리
-INSERT INTO good(goodno, memberno, contentsno, rdate)
-VALUES(good_seq.nextval, 3, 1, SYSDATE);
+-- 콘텐츠의 모든 조회수
+SELECT COUNT(*) as cnt FROM likes
+WHERE contentsno=1;
 
---1) 1번 회원 로그인시 관심분야를 가져오는 SQL
-   SELECT fcateno FROM frecommend WHERE memberno=1
+-- 1번 회원이 2번 상품을 좋아요 클릭했는지 카운트, 1: 좋아요했음, 0은 자료 없음
+SELECT COUNT(*) as cnt 
+FROM likes
+WHERE contentsno=1 AND memberno=3;
 
---2) 가장 추천(좋아요)을 많이 받은 상품 10건 출력
--- 관심분야의 카테고리 1번 상품중에 recom(추천수) 컬럼의 내림차순 정렬후 상품 목록 6건을 출력 
+-- 삭제
+DELETE FROM likes WHERE contentsno=2 and memberno=3;  
 
-SELECT contentsno, adminno, fcateno, title, content, recom, cnt, replycnt, rdate,
-           file1, file1saved, thumb1, size1, map, youtube, r
-FROM (
-           SELECT contentsno, adminno, fcateno, title, content, recom, cnt, replycnt, rdate,
-                      file1, file1saved, thumb1, size1, map, youtube, rownum as r
-           FROM (
-                     SELECT contentsno, adminno, fcateno, title, content, recom, cnt, replycnt, rdate,
-                                file1, file1saved, thumb1, size1, map, youtube
-                     FROM festival
-                     WHERE fcateno=7 AND (title LIKE '%서울%' OR content LIKE '%서울%' OR word LIKE '%서울%')
-                     ORDER BY recom DESC
-           )          
-)
-WHERE r >= 1 AND r <= 6;
+commit;
