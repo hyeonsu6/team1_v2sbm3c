61e3630cfdc95b99b97f75b1112a44507633d6f4
hyeonsu6 <bhshs0508@naver.com>
2023-12-30 22:07:15 +0900

    2차 업데이트

--- src/main/webapp/WEB-INF/doc/erd/전체/review_reply.sql
+++ src/main/webapp/WEB-INF/doc/erd/전체/review_reply.sql
@@ -1,26 +1,26 @@
 /**********************************/
-/* Table Name: 리뷰댓글 */
+/* Table Name: 리뷰댓글             */
 /**********************************/
 DROP TABLE FREVIEW_REPLY;
 
 CREATE TABLE FREVIEW_REPLY(
-		REPLYNO     NUMERIC(10)     NOT NULL PRIMARY KEY,
+		replyno     NUMERIC(10)     NOT NULL PRIMARY KEY,
 		reviewno    NUMBER(10)      NOT NULL,
 		id          VARCHAR(30)     NOT NULL,
-		REPLY       VARCHAR(150)    NOT NULL,
-		PASSWD      VARCHAR(10)     NOT NULL,
-		RDATE       DATE            NOT NULL,
+		reply       VARCHAR(150)    NOT NULL,
+		passwd      VARCHAR(10)     NOT NULL,
+		rdate       DATE            NOT NULL,
   FOREIGN KEY (id) REFERENCES MEMBER (id),
   FOREIGN KEY (reviewno) REFERENCES FREVIEW (reviewno)
 );
 
 COMMENT ON TABLE FREVIEW_REPLY is '리뷰 댓글';
-COMMENT ON COLUMN FREVIEW_REPLY.REPLYNO is '댓글번호';
+COMMENT ON COLUMN FREVIEW_REPLY.replyno is '댓글번호';
 COMMENT ON COLUMN FREVIEW_REPLY.reviewno is '리뷰번호';
 COMMENT ON COLUMN FREVIEW_REPLY.id is '회원 아이디';
-COMMENT ON COLUMN FREVIEW_REPLY.REPLY is '댓글내용';
-COMMENT ON COLUMN FREVIEW_REPLY.PASSWD is '패스워드';
-COMMENT ON COLUMN FREVIEW_REPLY.RDATE is '등록일';
+COMMENT ON COLUMN FREVIEW_REPLY.reply is '댓글내용';
+COMMENT ON COLUMN FREVIEW_REPLY.passwd is '패스워드';
+COMMENT ON COLUMN FREVIEW_REPLY.rdate is '등록일';
 
 DROP SEQUENCE FREVIEW_REPLY_SEQ;
 
@@ -36,30 +36,31 @@ COMMIT;
 
 /*******************************************************************************/
 
--- CREATE
-INSERT INTO FREVIEW_REPLY(REPLYNO, reviewno, id, REPLY, PASSWD, RDATE)
-VALUES (FREVIEW_REPLY_SEQ.nextval, 6, 'user1@gmail.com', '리뷰보고 다녀왔는데 너무 재밌었어요~!', '1234', sysdate);
-INSERT INTO FREVIEW_REPLY(REPLYNO, reviewno, id, REPLY, PASSWD, RDATE)
-VALUES (FREVIEW_REPLY_SEQ.nextval, 6, 'user1@gmail.com', '추천해요~!! 완전 재밌어요~', '1234', sysdate);
-INSERT INTO FREVIEW_REPLY(REPLYNO, reviewno, id, REPLY, PASSWD, RDATE)
-VALUES (FREVIEW_REPLY_SEQ.nextval, 6, 'user1@gmail.com', '사람 엄청 많아요! 조심하세요~', '1234', sysdate);
+-- INSERT, 데이터 삽입
+INSERT INTO FREVIEW_REPLY(replyno, reviewno, id, reply, passwd, rdate)
+VALUES (FREVIEW_REPLY_SEQ.NEXTVAL, 6, 'user1@gmail.com', '리뷰보고 다녀왔는데 너무 재밌었어요~!', '1234', sysdate);
+INSERT INTO FREVIEW_REPLY(replyno, reviewno, id, reply, passwd, rdate)
+VALUES (FREVIEW_REPLY_SEQ.NEXTVAL, 6, 'user1@gmail.com', '추천해요~!! 완전 재밌어요~', '1234', sysdate);
+INSERT INTO FREVIEW_REPLY(NEXTVAL, reviewno, id, reply, passwd, rdate)
+VALUES (FREVIEW_REPLY_SEQ.NEXTVAL, 6, 'user1@gmail.com', '사람 엄청 많아요! 조심하세요~', '1234', sysdate);
+
+COMMIT; 
 
 
--- READ
-SELECT REPLYNO, reviewno, id, REPLY, PASSWD, RDATE FROM FREVIEW_REPLY ORDER BY REPLYNO ASC;
-SELECT REPLYNO, reviewno, id, REPLY, PASSWD, RDATE FROM FREVIEW_REPLY WHERE reviewno = 6 ORDER BY REPLYNO ASC;
-SELECT REPLYNO, reviewno, id, REPLY, PASSWD, RDATE FROM FREVIEW_REPLY WHERE id = 'user2@gmail.com' ORDER BY REPLYNO ASC;
+-- READ, 목록&조회   
+SELECT replyno, reviewno, id, reply, passwd, rdate FROM FREVIEW_REPLY ORDER BY replyno ASC;
+SELECT replyno, reviewno, id, reply, passwd, rdate FROM FREVIEW_REPLY WHERE reviewno = 6 ORDER BY replyno ASC;
+SELECT replyno, reviewno, id, reply, passwd, rdate FROM FREVIEW_REPLY WHERE id = 'user2@gmail.com' ORDER BY replyno ASC;
 
 
--- UPDATE
-UPDATE FREVIEW_REPLY SET REPLY = '리뷰보고 다녀왔는데 너무 재밌었어요~~~!' WHERE REPLYNO = 1;
+-- UPDATE, 수정
+UPDATE FREVIEW_REPLY SET reply = '리뷰보고 다녀왔는데 너무 재밌었어요~~~!' WHERE replyno = 1;
 
+COMMIT; 
 
--- DELETE
-DELETE FROM FREVIEW_REPLY WHERE REPLYNO = 8 AND PASSWD = '1234';
+-- DELETE, 삭제
+DELETE FROM FREVIEW_REPLY WHERE replyno = 8 AND passwd = '1234';
 
-
-/*******************************************************************************/
 COMMIT; 
 
 
