72b8c78b2d839d56d5bbd9bddda6d4b3715191c8
hyeonsu6 <bhshs0508@naver.com>
2024-01-03 18:58:00 +0900

    AdminContRest 리액트

--- src/main/java/dev/mvc/team1_v2sbm3c/WebMvcConfiguration.java
+++ src/main/java/dev/mvc/team1_v2sbm3c/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package dev.mvc.team1_v2sbm3c;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,9 +20,9 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 		// ▶ file:////home/ubuntu/deploy/resort_v2sbm3c_blog/contents/storage
 
 		// JSP 인식되는 경로: http://localhost:9093/contents/storage";
-		registry.addResourceHandler("/festival/storage/**").addResourceLocations("file:///" + Festival.getUploadDir());
-		// JSP 인식되는 경로: http://localhost:9093/contents/storage";
-		registry.addResourceHandler("/freview/storage/**").addResourceLocations("file:///" + Freview.getUploadDir());
+//		registry.addResourceHandler("/festival/storage/**").addResourceLocations("file:///" + Festival.getUploadDir());
+//		// JSP 인식되는 경로: http://localhost:9093/contents/storage";
+//		registry.addResourceHandler("/freview/storage/**").addResourceLocations("file:///" + Freview.getUploadDir());
 
 		// JSP 인식되는 경로: http://localhost:9091/attachfile/storage";
 		// registry.addResourceHandler("/contents/storage/**").addResourceLocations("file:///"
@@ -32,4 +33,9 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 		// + Tool.getOSPath() + "/member/storage/");
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOriginPatterns("*") // React 앱의 URL, 2.4 부터 allowedOriginPatterns 사용
+				.allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(true);
+	}
 }
