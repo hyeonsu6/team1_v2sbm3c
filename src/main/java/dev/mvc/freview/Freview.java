2a8c5dcd574fcbeaccbd29b76ab5d5dd9fb1301b
hyeonsu6 <bhshs0508@naver.com>
2024-01-04 19:08:05 +0900

    img 에러 수정

--- src/main/java/dev/mvc/freview/Freview.java
+++ src/main/java/dev/mvc/freview/Freview.java
@@ -17,7 +17,7 @@ public class Freview {
      String path = "";
      if (File.separator.equals("\\")) { // windows, 개발 환경의 파일 업로드 폴더
          // path = "C:/kd/deploy/team1_v4sbm3c/contents/storage/";
-         path="C:\\kd\\deploy\\team1_v2sbm3c\\freview\\storage";
+         path="C:\\kd\\deploy\\team1_v2sbm3c\\freview\\storage\\";
          // System.out.println("Windows 10: " + path);
          
      } else { // Linux, AWS, 서비스용 배치 폴더 
